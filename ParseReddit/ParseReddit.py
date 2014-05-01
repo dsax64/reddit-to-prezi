449a7a5fc2f3482c12590ceb9886e623c5047e4c
Balazs Szele <balazs.szele@prezi.com>
2014-05-01 17:37:42 +0200

    Mark functions as internal use

--- ParseReddit/ParseReddit.py
+++ ParseReddit/ParseReddit.py
@@ -8,45 +8,45 @@ class ParseReddit(object):
         self.reddit_url = reddit_url
 
     def get_reddit_dict(self):
-        return self.parse_url()
+        return self._parse_url()
 
-    def parse_url(self):
-        soup_parsed_data = self.get_html_soup_data()
-        commentarea = self.get_comment_thread(soup_parsed_data)
-        return self.get_comments(commentarea)
+    def _parse_url(self):
+        soup_parsed_data = self._get_html_soup_data()
+        commentarea = self._get_comment_thread(soup_parsed_data)
+        return self._get_comments(commentarea)
 
-    def get_html_soup_data(self):
+    def _get_html_soup_data(self):
         response = urllib2.urlopen(self.reddit_url)
         html = response.read()
         soup_parsed_data = BeautifulSoup(html)
         return soup_parsed_data
 
-    def get_comment_thread(self, thread, thread_type='commentarea'):
+    def _get_comment_thread(self, thread, thread_type='commentarea'):
         return thread.find("div", class_=thread_type)
 
-    def get_comments(self, thread):
+    def _get_comments(self, thread):
         if thread.contents:
-            return self.parse_thread(thread)
+            return self._parse_thread(thread)
         else:
             return None
 
-    def parse_thread(self, thread):
+    def _parse_thread(self, thread):
         dict = {}
         i = 0
-        comments = self.get_comments_table(thread)
+        comments = self._get_comments_table(thread)
         for comment in comments:
-            entry, child = self.parse_single_comment(comment)
-            dict[i] = {'entry': entry, 'child': self.get_comments(child)}
+            entry, child = self._parse_single_comment(comment)
+            dict[i] = {'entry': entry, 'child': self._get_comments(child)}
             i += 1
         return dict
 
-    def get_comments_table(self, thread):
+    def _get_comments_table(self, thread):
         site_table = thread.find("div", class_="sitetable")
         comments_table = site_table.find_all("div", class_="comment", recursive=False)
         return comments_table
 
-    def parse_single_comment(self, thread_entry):
+    def _parse_single_comment(self, thread_entry):
             entry = thread_entry.find("div", class_="entry").find(
                 "div", class_="md").get_text('\n').rstrip().encode('utf-8')
-            child = self.get_comment_thread(thread_entry, 'child')
+            child = self._get_comment_thread(thread_entry, 'child')
             return entry, child
