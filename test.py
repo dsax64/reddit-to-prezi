d2f13181ae627d07134b54ca9f22e29b39bc21b0
akoskaaa <akos.hochrein@prezi.com>
2014-05-02 15:14:17 +0200

    refreshed testcase to be smaller

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 from ParseReddit import ParseReddit
 from xml_generator.xml_generator import XmlGenerator
 
-pr = ParseReddit.ParseReddit('http://www.reddit.com/r/dadjokes/comments/24gmom/dad_joke_about_yoga/')
+pr = ParseReddit.ParseReddit('http://www.reddit.com/r/Python/comments/24id80/any_quirky_python_projects_to_hack_on/')
 reddit_dict = pr.get_reddit_dict()
 
 xml_generator = XmlGenerator(reddit_dict)
