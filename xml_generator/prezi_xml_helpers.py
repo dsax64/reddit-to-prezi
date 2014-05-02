eb6e2b6236054a0abc2e6e5070b3145b8831113a
akoskaaa <akos.hochrein@prezi.com>
2014-05-02 15:14:50 +0200

    massive refactor, still ugly though

--- xml_generator/prezi_xml_helpers.py
+++ xml_generator/prezi_xml_helpers.py
@@ -2,6 +2,7 @@
 from lxml import etree
 
 
+# TODO akos.hochrein think a way of removing side effects
 class PreziXmlHelpers(object):
 
     @staticmethod
@@ -20,3 +21,74 @@ class PreziXmlHelpers(object):
     def generate_version_node(xml_node, version):
         version_node = etree.SubElement(xml_node, 'version')
         version_node.text = str(version)
+
+    @staticmethod
+    def generate_type_node(xml_node, type):
+        type_node = etree.SubElement(xml_node, 'type')
+        type_node.text = type
+
+    @staticmethod
+    def generate_size_node(xml_node, dimensions):
+        size_node = etree.SubElement(xml_node, 'size')
+        w_node = etree.SubElement(size_node, 'w')
+        w_node.text = str(dimensions['w'])
+        h_node = etree.SubElement(size_node, 'h')
+        h_node.text = str(dimensions['h'])
+
+    @staticmethod
+    def generate_width_node(xml_node, dimensions):
+        w_node = etree.SubElement(xml_node, 'width')
+        w_node.text = str(dimensions['w'])
+
+    @staticmethod
+    def generate_height_node(xml_node, dimensions):
+        h_node = etree.SubElement(xml_node, 'height')
+        h_node.text = str(dimensions['h'])
+
+    @staticmethod
+    def generate_text_node(xml_node, text, alignment):
+        paragraph_node = etree.SubElement(xml_node, 'p', align=alignment)
+        text_node = etree.SubElement(paragraph_node, 'text')
+        text_node.text = text
+
+    @staticmethod
+    def generate_object_node(xml_node):
+        pass
+
+
+class ObjectNode(object):
+
+    def __init__(self, parent_node, id, node_type, dimensions):
+        return etree.SubElement(parent_node, 'object',
+                                id=str(id),
+                                type=str(node_type),
+                                x=str(dimensions['x']),
+                                y=str(dimensions['y']),
+                                s=str(dimensions['s']))
+
+
+class FrameNode(ObjectNode):
+
+    def __init__(self, parent_node, id, node_type, dimensions, frame_type):
+        frame_node = super(FrameNode, self).__init__(parent_node, id, node_type, dimensions)
+        PreziXmlHelpers.generate_type_node(frame_node, frame_type)
+        PreziXmlHelpers.generate_size_node(frame_node, dimensions)
+
+
+class TextNode(ObjectNode):
+
+    def __init__(self, parent_node, id, node_type, dimensions, text, alignment):
+        text_node = super(TextNode, self).__init__(parent_node, id, node_type, dimensions)
+        PreziXmlHelpers.generate_width_node(text_node, dimensions)
+        PreziXmlHelpers.generate_height_node(text_node, dimensions)
+        PreziXmlHelpers.generate_text_node(text_node, text, alignment)
+
+
+class ImageNode(ObjectNode):
+    pass
+
+
+class Thread(object):
+
+    def __init__(self):
+        pass
--- xml_generator/xml_generator.py
+++ xml_generator/xml_generator.py
@@ -1,6 +1,6 @@
 
 from lxml import etree
-from prezi_xml_helpers import PreziXmlHelpers
+from prezi_xml_helpers import PreziXmlHelpers, FrameNode, TextNode
 
 import random
 
@@ -8,67 +8,53 @@ import random
 class XmlGenerator():
 
     def __init__(self, data):
-        self.data = data
-        self.xml_data = etree.Element('zuiprezi')
-        self.zui_table = etree.SubElement(self.xml_data, 'zui-table')
+        self.thread = data
+        self.xml_thread = etree.Element('zuiprezi')
+        self.zui_table = etree.SubElement(self.xml_thread, 'zui-table')
         self.counter = 0
 
     def generate_xml(self):
         self._initiate_base_xml()
-        self._parse_json_object(self.data)
-        return etree.tostring(self.xml_data)
+        self._parse_json_object(self.thread)
+        return etree.tostring(self.xml_thread)
 
     def _initiate_base_xml(self):
-        PreziXmlHelpers.generate_version_node(self.xml_data, 1)
+        PreziXmlHelpers.generate_version_node(self.xml_thread, 1)
         PreziXmlHelpers.generate_autoplay_node(self.zui_table, 4000)
 
         # TODO figure out a bound algorithm
-        # TODO make this more descriptive
+        # TODO akos.hochrein create a Dimensions class
         PreziXmlHelpers.generate_bounds_node(self.zui_table,
                                              (0.0, 0.0),
                                              (100.0, 100.0))
 
-    def _parse_json_object(self, data):
-        self._parse_thread(data)
+    def _parse_json_object(self, thread):
+        self._parse_thread(thread)
 
     # TODO omfg refactor
-    def _parse_thread(self, data):
-        if isinstance(data, dict):
-            for key in data.keys():
+    def _parse_thread(self, thread):
+        if isinstance(thread, dict):
+            for key in thread.keys():
                 if key == 'entry':
                     current_id = '0_' + str(random.randint(1, 100000))
-                    frame_node = etree.SubElement(self.zui_table, 'object',
-                                                  id=current_id,
-                                                  x=str((self.counter + 1) * 8000),
-                                                  y="0",
-                                                  s="10",
-                                                  type='button')
-                    type_node = etree.SubElement(frame_node, 'type')
-                    type_node.text = 'circle'
-                    size_node = etree.SubElement(frame_node, 'size')
-                    w_node = etree.SubElement(size_node, 'w')
-                    w_node.text = '800'
-                    h_node = etree.SubElement(size_node, 'h')
-                    h_node.text = '800'
-
-                    text_node = etree.SubElement(self.zui_table, 'object',
-                                                 id=current_id,
-                                                 x=str((self.counter + 1) * 8000),
-                                                 y="0",
-                                                 s="18",
-                                                 type='text')
-                    w_node = etree.SubElement(text_node, 'width')
-                    w_node.text = '220'
-                    h_node = etree.SubElement(text_node, 'height')
-                    h_node.text = '28'
-
-                    p_node = etree.SubElement(text_node, 'p',
-                                              align='center')
-                    t_node = etree.SubElement(p_node, 'text')
-
-                    t_node.text = data[key].decode('utf-8')
+                    dimensions = {'x': self.counter * 8000,
+                                  'y': 0,
+                                  's': 10 / float(self.counter + 1),
+                                  'w': 800,
+                                  'h': 800}
+                    FrameNode(self.zui_table,
+                              current_id,
+                              'button',
+                              dimensions,
+                              'circle')
+                    TextNode(self.zui_table,
+                             current_id,
+                             'text',
+                             dimensions,
+                             thread[key].decode('utf-8'),
+                             'center')
                     self.counter += 1
-                self._parse_thread(data[key])
+                self._parse_thread(thread[key])
 
 if __name__ == '__main__':
     xml_generator = XmlGenerator({"0": {"child": {"0": {"child": {}, "entry": "test text 10"}, "1": {"child": {}, "entry": "test"}}, "entry": "test text 00"}})
